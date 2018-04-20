e38572112fc9487a4fd17de64576fa78e7491b59
Anshuma <anshuma@Anshumas-MacBook-Air.local>
2018-04-19 21:09:06 -0400

    NB + DT

--- lib/DT.R
+++ lib/DT.R
@@ -13,21 +13,5 @@ prp(tree)
 prediction <- predict(tree, test_data, type = "class")
 table(prediction,test_data$cuisine)
 
-printALL=function(model){
-  trainPred=predict(model, newdata = train_data, type = "class")
-  trainTable=table(train_data$cuisine, trainPred)
-  testPred=predict(model, newdata=test_data, type="class")
-  testTable=table(test_data$cuisine, testPred)
-  trainAcc=(trainTable[1,1]+trainTable[2,2]+trainTable[3,3])/sum(trainTable)
-  testAcc=(testTable[1,1]+testTable[2,2]+testTable[3,3])/sum(testTable)
-  message("Contingency Table for Training Data")
-  print(trainTable)
-  message("Contingency Table for Test Data")
-  print(testTable)
-  message("Accuracy")
-  print(round(cbind(trainAccuracy=trainAcc, testAccuracy=testAcc),3))
-}
-printALL(tree)
-
-#trainAccuracy testAccuracy
-#         0.012        0.011 
+mean(prediction!=train_data[,1]) #0.870533
+mean(prediction!=test_data[,1])  #0.5555109
