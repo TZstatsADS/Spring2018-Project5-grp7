9cf39370b4f65dc8470f6d8306f5a7fa179a80eb
Anshuma <anshuma@dyn-209-2-210-201.dyn.columbia.edu>
2018-04-20 18:13:01 -0400

    code

--- lib/NB.R
+++ lib/NB.R
@@ -8,5 +8,5 @@ NB_model<-naiveBayes(as.factor(cuisine)~.,data=train_data)
 prediction_NB <- predict(NB_model, test_data[,-1],type="class")
 table(prediction_NB,test_data$cuisine)
 mean(prediction_NB!=test_data[,1]) # 0.6523532
-mean(prediction_NB!=train_data[,1]) # 0.9329199
+
 
